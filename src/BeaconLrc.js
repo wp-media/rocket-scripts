60279b503bc4ab71fe5fd9279c20e6ade88cb768
WordPressFan <ahmed@wp-media.me>
2024-08-16 14:35:19 +0300

    start adding the beaconlrc class

---
+++ src/BeaconLrc.js
@@ -0,0 +1,135 @@
+'use strict';
+
+import BeaconUtils from "./Utils.js";
+import BeaconManager from "./BeaconManager.js";
+
+class BeaconLrc {
+    constructor(config, logger) {
+        this.config = config;
+        this.logger = logger;
+        this.lazyRenderElements = [];
+    }
+
+    async run() {
+        try {
+            const elementsInView = this._getLazyRenderElements();
+            if (elementsInView) {
+                this._processElements(elementsInView);
+            }
+        } catch (err) {
+            this.errorCode = 'script_error';
+            this.logger.logMessage('Script Error: ' + err);
+        }
+    }
+
+    _getLazyRenderElements() {
+        const elements = document.querySelectorAll(this.config.elements);
+
+        if (elements.length <= 0) {
+            return [];
+        }
+
+        const validElements = Array.from(elements).filter(element => !this._skipElement(element));
+
+        return validElements.map(element => ({
+            element: element,
+            depth: this._getElementDepth(element),
+            distance: this._getElementDistance(element)
+        }));
+    }
+
+    _getElementDepth(element) {
+        let depth = 0;
+        let parent = element.parentElement;
+        while (parent) {
+            depth++;
+            parent = parent.parentElement;
+        }
+        return depth;
+    }
+
+    _getElementDistance(element) {
+        const rect = element.getBoundingClientRect();
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return rect.top + scrollTop - (window.innerHeight || document.documentElement.clientHeight);
+    }
+
+    _skipElement(element) {
+        const skipStrings = this.config.skipStrings || ['memex'];
+        if (!element || !element.id) return false;
+        return skipStrings.some(str => element.id.toLowerCase().includes(str));
+    }
+
+    _shouldSkipElement(element, exclusions) {
+        if (!element) return false;
+        for (let i = 0; i < exclusions.length; i++) {
+            const [attribute, pattern] = exclusions[i];
+            const attributeValue = element.getAttribute(attribute);
+            if (attributeValue && new RegExp(pattern, 'i').test(attributeValue)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    _processElements(elements) {
+        elements.forEach(({ element, depth, distance }) => {
+            if (this._shouldSkipElement(element, this.config.exclusions || [])) {
+                return;
+            }
+
+            this.lazyRenderElements.push({ element, depth, distance });
+
+            const style = distance > 1800 ? 'color: green;' : distance === 0 ? 'color: red;' : '';
+            console.log(`%c${'\t'.repeat(depth)}${element.tagName} (Depth: ${depth}, Distance from viewport top: ${distance}px)`, style);
+
+            const xpath = this._getXPath(element);
+            console.log(`%c${'\t'.repeat(depth)}Xpath: ${xpath}`, style);
+
+            const locationhash = this._getLocationHash(element);
+            console.log(`%c${'\t'.repeat(depth)}Location hash: ${locationhash}`, style);
+
+            console.log(`%c${'\t'.repeat(depth)}Dimensions Client Height: ${element.clientHeight}`, style);
+        });
+    }
+
+    _getXPath(element) {
+        if (element.id !== "") {
+            return `//*[@id="${element.id}"]`;
+        }
+
+        return this._getElementXPath(element);
+    }
+
+    _getElementXPath(element) {
+        if (element === document.body) {
+            return '/html/body';
+        }
+        const position = this._getElementPosition(element);
+        return `${this._getElementXPath(element.parentNode)}/${element.nodeName.toLowerCase()}[${position}]`;
+    }
+
+    _getElementPosition(element) {
+        let pos = 1;
+        let sibling = element.previousElementSibling;
+        while (sibling) {
+            if (sibling.nodeName === element.nodeName) {
+                pos++;
+            }
+            sibling = sibling.previousElementSibling;
+        }
+        return pos;
+    }
+
+    _getLocationHash(element) {
+        return element.hasAttribute('data-rocket-location-hash')
+            ? element.getAttribute('data-rocket-location-hash')
+            : 'No hash detected';
+    }
+
+    getResults() {
+        return this.lazyRenderElements;
+    }
+}
+
+export default BeaconLrc;
