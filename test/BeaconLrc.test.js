ae2ec053cc6f6c698e4a2600069181e958311d2d
Opeyemi Ibrahim <opeyemi.khadri@gmail.com>
2024-10-07 14:52:21 +0100

    Fixed codacy error

--- test/BeaconLrc.test.js
+++ test/BeaconLrc.test.js
@@ -4,8 +4,7 @@ import sinon from "sinon";
 
 describe('BeaconLrc', function() {
     let beaconLrc;
-    let mockElements,
-        lrcMockElements;
+    let mockElements;
 
     beforeEach(function() {
         mockElements = [
